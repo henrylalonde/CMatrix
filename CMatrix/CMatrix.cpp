06e820916e493918bd207b3d0287d979c477e2dc
henrylalonde <henryplalonde1@gmail.com>
2025-02-13 18:11:12 -0600

    Add project files.

---
+++ CMatrix/CMatrix.cpp
@@ -0,0 +1,17 @@
+﻿// CMatrix.cpp : Defines the entry point for the application.
+//
+
+#include "CMatrix.h"
+
+int main()
+{
+	Eigen::Matrix4d m;
+
+	m << 1, 2, 3, 4,
+		2, 3, 4, 5,
+		3, 4, 5, 6,
+		4, 5, 6, 7;
+
+	std::cout << m << std::endl;
+	return 0;
+}
